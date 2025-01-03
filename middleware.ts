8adc7cc6489cdbc83a987c6f7edf0d90aad52945
jayshiai <jayvardhanpatil7811@gmail.com>
2025-01-04 00:08:56 +0530

    asd

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 
 import { updateSession } from './utils/supabase/middleware'
 import { NextResponse, type NextRequest } from 'next/server'
+
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  let hostname = req.headers.get("host")!.replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  let hostname = req.headers.get("host")!;
 
   // Special case for Vercel preview deployment URLs
   if (hostname.includes("---") && hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)) {
@@ -13,34 +14,34 @@ export async function middleware(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams.toString();
   const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
 
-  // 1. Apply session update middleware for specific paths
+  // Apply session update middleware for specific paths
   if (path.startsWith("/dashboard") || path.startsWith("/editor") || path === "/login" || path === "/register") {
     return await updateSession(req);
   }
 
+  // Rewrite based on the hostname and ensure production domains are properly used
+  if (hostname === `localhost:3000`) {
+    // Development environment: Allow local URLs
+    return NextResponse.rewrite(new URL(`${path}`, req.url));
+  }
+
+  // Production environment: Rewrite URLs to the correct domain
+  if (hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
+    return NextResponse.rewrite(new URL(`/${path}`, req.url));
+  }
+
+  // Check for the domain with UI prefix, and rewrite accordingly
   if (hostname === `ui.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
-
     return NextResponse.rewrite(new URL(`/`, req.url));
-  }
-
-  // Special case for `vercel.pub` domain
-  if (hostname === "vercel.pub") {
-    return NextResponse.redirect("https://vercel.com/blog/platforms-starter-kit");
-  }
-
-  // Rewrite root application to `/home` folder for specific domains
-  if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
-    return NextResponse.rewrite(new URL(`${path === "/" ? "" : path}`, req.url));
   }
 
   // Default rewrite for other domains: `/[domain]/[slug]`
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
 
-
 export const config = {
-  matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register",  "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",],
-}
+  matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register", "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
+};
 
 
 
